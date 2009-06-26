5daa39805bc72d876eac69365cea67bf4513fc73
anoochit <anoochit@5ac04c73-d94c-0410-b87e-dbe68a168716>
2009-06-26 03:56:36 +0000

    commit chantra disc structure

---
+++ branches/ChantraBuilder/disc/defaults/preferences/mybrowser-prefs.js
@@ -0,0 +1,4 @@
+pref("toolkit.defaultChromeURI", "chrome://interfacewiki/content/interfacewiki.xul");
+pref("general.skins.selectedSkin", "theopendisc");
+pref("intl.charset.default", "UTF-8");
+pref("network.protocol-handler.warn-external.http", false);
