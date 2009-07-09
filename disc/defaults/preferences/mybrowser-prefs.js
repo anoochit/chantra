bc0e07a5c6bfffbb5072118e2b63d4d0dc8db7aa
anoochit <anoochit@5ac04c73-d94c-0410-b87e-dbe68a168716>
2009-07-09 01:08:02 +0000

    add default profile for adhoc file access by default application

--- disc/defaults/preferences/mybrowser-prefs.js
+++ disc/defaults/preferences/mybrowser-prefs.js
@@ -2,3 +2,10 @@ pref("toolkit.defaultChromeURI", "chrome://interfacewiki/content/interfacewiki.x
 pref("general.skins.selectedSkin", "theopendisc");
 pref("intl.charset.default", "UTF-8");
 pref("network.protocol-handler.warn-external.http", false);
+pref("network.protocol-handler.warn-external.https", false);
+pref("network.protocol-handler.warn-external.ftp", false);
+pref("network.protocol-handler.external-default", true);
+pref("browser.places.importBookmarksHTML", false);
+pref("browser.places.importDefaults", false);
+pref("browser.places.leftPaneFolderId", -1);
+pref("browser.places.smartBookmarksVersion", 0);
