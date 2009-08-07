0d48be672760f22066e4b5130554cea0c90fa386
anoochit <anoochit@5ac04c73-d94c-0410-b87e-dbe68a168716>
2009-08-07 10:21:29 +0000

    replace builder script

---
+++ trunk/tools/tool-touch-programs.cli.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?php
+ 
+$fp=file("../software.list.txt");
+foreach ($fp as $item) {
+    $item=trim($item);
+    if (!empty($item)){
+        /*
+	$fp=fopen("../short-desc/".$item,"w");
+        fwrite($fp,"<description>\n");
+        fclose($fp);
+	*/
+	mkdir("../programs/".$item,0777);
+    }
+}
+ 
+ 
+?>
